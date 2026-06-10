ea09f88ba6d01145a409b8f08f2976a3c7b63369
tradewithrajeevshivah-collab <tradewithrajeevshivah@gmail.com>
2026-06-10 18:19:51 +0530

    Fix: remove timeouts, add tls rejectUnauthorized false -1

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -12,6 +12,9 @@ const getTransporter = () => nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 // ---- Booking confirmed (Razorpay) ----
